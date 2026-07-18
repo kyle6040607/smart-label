4b65d58fd58946c6cc876a54736830aa628e669b
dumpling871008 <dumpling8877@gmail.com>
2026-07-18 15:32:56 +0800

    修改dockerfile設定 準備cloud run

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ app = create_app()
 if __name__ == "__main__":
     app.run(
         host=os.getenv("HOST", "0.0.0.0"),
-        port=int(os.getenv("PORT", "5000")),
+        port=int(os.getenv("PORT", "8080")),
         debug=os.getenv("DEBUG", "1") == "1",
     )
